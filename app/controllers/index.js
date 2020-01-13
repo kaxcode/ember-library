6ee489bc015f07cb02960a45fab4ffbc7f5150c8
kaxcode <keniavalladarez@gmail.com>
2020-01-13 14:28:09 -0500

    Creates and index controller with some computed properties.

---
+++ app/controllers/index.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
+
+export default Controller.extend({
+  emailAddress: '',
+
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+});
