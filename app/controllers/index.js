a19e8f6b8544a1e66ae41e2892ccd53385394300
kaxcode <keniavalladarez@gmail.com>
2020-01-15 09:43:59 -0500

    Refactors to use shorthand prperty names and arrow functions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,14 +13,14 @@ export default Controller.extend({
     saveInvitation() {
       const email = this.get('emailAddress');
 
-      const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
-
-      this.set(
-        'responseMessage',
-        `Thank you! We have just saved your email address: ${this.get('emailAddress')}`
-      );
-      this.set('emailAddress', '');
+      const newInvitation = this.store.createRecord('invitation', { email });
+      newInvitation.save().then(response => {
+        this.set(
+          'responseMessage',
+          `Thank you! We have just saved your email address: ${this.get('emailAddress')}`
+        );
+        this.set('emailAddress', '');
+      });
     },
   },
 });
