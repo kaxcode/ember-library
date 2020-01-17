e458f6d02028dcc58d408217fc84aa9a4de481bc
kaxcode <19161242+kaxcode@users.noreply.github.com>
2020-01-17 18:10:32 -0500

    Updates contact route to save new contact in server and for the temlate to use model instead of controller

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,3 +1,19 @@
 import Route from '@ember/routing/route';
 
-export default class ContactRoute extends Route {}
+export default Route.extend({
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+    saveContact(newContact) {
+      newContact.save();
+    },
+
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+    },
+  },
+});
